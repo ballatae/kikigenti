4c9b141563cd07ac93c3bcdec1e8199f75210a96
ballatae <92359676+ballatae@users.noreply.github.com>
2021-12-07 09:53:58 +0200

    Add files via upload

---
+++ NumberMethods.java
@@ -0,0 +1,77 @@
+package functions;
+public class NumberMethods {
+    public static int minimumInt(int num1, int num2) {
+        if(num1 < num2) {
+        	System.out.println(num1);
+        }else if(num2 <= num1) {
+        	System.out.println(num2);
+        }
+        
+    }
+
+    public static int maximumInt(int num1, int num2){
+    	if(num1 > num2) {
+        	System.out.println(num1);
+        }else if(num2 >= num1) {
+        	System.out.println(num2);
+        }
+    }
+
+    public static double minimumDouble(double num1, double num2){
+
+    }
+
+    public static double maximumInt(double num1, double num2){
+
+    }
+
+    // ! --------
+    
+    public static int median(int num1, int num2, int num3){
+
+    }
+
+    public static int average(int num1, int num2, int num3, int num4, int num5){
+
+    }
+
+    public static int isEven(int num1){
+
+    }
+
+    // ! -----------
+
+    public static int sumAllNumbersUpTo(int lastNumber){
+
+    }
+
+    public static int sumEvenNumbersUpTo( int lastEvenNumber){
+
+    }
+
+    public static int percentage(int num1, int percentageOfNum1){
+
+    }
+
+    public static int solveEquation(int numA, int numB){
+
+    }
+
+    public static int squarePerimeter(int sideLength){
+
+    }
+
+    public static int triangleArea(int base, int height){
+
+    }
+
+    public static int sumOfDigits(int num1){
+
+    }
+    
+    public static int reverseANum(int twoDigitnum){
+
+    }
+
+
+}
